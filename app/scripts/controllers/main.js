d5c60ac9b0518876310863d88be940bb3d1fcd13
quentin22800 <24philippe>
2018-04-06 19:00:33 +0200

    ajout get et post people

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,5 +1,5 @@
 'use strict';
-
+var api = angular.module('tpsirApp');
 /**
  * @ngdoc function
  * @name tpsirApp.controller:MainCtrl
@@ -7,11 +7,27 @@
  * # MainCtrl
  * Controller of the tpsirApp
  */
-angular.module('tpsirApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+api.config(['$resourceProvider', function ($resourceProvider) {
+  $resourceProvider.defaults.stripTrailingSlashes = false;
+}]);
+
+api.factory('getPeopleService', ['$resource', function ($resource) {
+  return $resource("http://localhost:9000/rest/domain/people");
+}]);
+
+api.controller('MainCtrl', ['$scope', '$log', 'getPeopleService','$http','$location', function ($scope, $log, getPeopleService, $http, $location) {
+  $scope.getPeopleService = getPeopleService;
+  var result = $scope.getPeopleService.query(function () {
+    $scope.peoples = result;
   });
+  $scope.savePeople = function(form){
+    if($scope.name != null && $scope.name.trim() != ""){
+      $http({
+        method: 'POST',
+        url: "http://localhost:9000/rest/domain/people?name="+$scope.name,
+      }).then(function (result) {
+      })
+    }
+  }
+}]);
+
